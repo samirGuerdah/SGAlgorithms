ada1c510e7921b7503b8fef44972ecf36a8fecd5
samirMSTF <sguerdah@mystudiofactory.com>
2015-11-12 11:44:13 +0100

    PushDown using linked list

--- PushdownStack.playground/Contents.swift
+++ PushdownStack.playground/Contents.swift
@@ -1,6 +1,4 @@
 
-import UIKit
-
 struct  PushdownStack<Element> {
 	
 	private var items = [Element]()
@@ -20,5 +18,46 @@ struct  PushdownStack<Element> {
 	
 	func size() -> Int {
 		return items.count
+	}
+}
+
+
+
+class Node <Item> {
+	
+	var item: Item
+	var next: Node?
+	
+	init(item : Item, next: Node?){
+		self.item = item
+		self.next = next
+	}
+}
+
+
+struct PushdownStackLinkedList<T> {
+	
+	var first : Node<T>?
+	var n = 0 // Number of elements
+	
+	mutating func push(item: T) {
+		n++
+		let newFirst = Node<T>.init(item: item, next: first)
+		first = newFirst
+	}
+	
+	mutating func pop() -> T? {
+		if n != 0 { n-- }
+		let firstNode = first
+		first = first?.next
+		return firstNode?.item
+	}
+	
+	func isEmpty() -> Bool {
+		return n == 0
+	}
+	
+	func size() -> Int {
+		return n;
 	}
 }
