82a140e6b51a6805161104d546756f25f488268d
samirMSTF <sguerdah@mystudiofactory.com>
2015-11-13 17:00:53 +0100

    Simple bag with liked list

--- Bag.playground/Contents.swift
+++ Bag.playground/Contents.swift
@@ -14,3 +14,36 @@ struct Bag<T: Hashable> {
 		return items.count
 	}
 }
+
+/**   Bag with a linked list **/
+class Node <Item> {
+	
+	var item: Item
+	var next: Node?
+	
+	init(item : Item, next: Node?){
+		self.item = item
+		self.next = next
+	}
+}
+
+struct BagLinkedList<T> {
+	
+	private var n = 0
+	private var first : Node<T>?
+	
+	mutating func add(item : T) {
+		let oldFirst = first
+		let newFirst = Node<T>.init(item: item, next: oldFirst)
+		first = newFirst
+		n++
+	}
+	
+	func isEmpty() -> Bool {
+		return n == 0
+	}
+	
+	func size() -> Int {
+		return n
+	}
+}
