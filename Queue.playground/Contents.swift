e0ed8fbd1ac33b838561cb91b0d3bfa43c6bb032
samirMSTF <sguerdah@mystudiofactory.com>
2015-11-13 16:12:08 +0100

    Queue with linked list

--- Queue.playground/Contents.swift
+++ Queue.playground/Contents.swift
@@ -21,3 +21,51 @@ struct Queue<T> {
 		return items.count
 	}
 }
+
+/**   Queue with a linked list **/
+class Node <Item> {
+	
+	var item: Item
+	var next: Node?
+
+	init(item : Item, next: Node?){
+		self.item = item
+		self.next = next
+	}
+}
+
+struct QueueLikedList<T> {
+	
+	private var first : Node<T>?
+	private var last : Node<T>?
+	private var n = 0
+	
+	mutating func enqueue(item : T) {
+		let newLastNode = Node<T>.init(item: item, next: nil)
+		let oldLast = last
+		if isEmpty() {
+			first = newLastNode
+			last = newLastNode
+		}
+		else{
+			oldLast?.next = newLastNode
+			last = newLastNode
+		}
+		n++
+	}
+	
+	mutating func dequeue() -> T? {
+		if n != 0 { n-- }
+		let firstNode = first
+		first = first?.next
+		return firstNode?.item
+	}
+	
+	func isEmpty() -> Bool {
+		return n == 0
+	}
+	
+	func size() -> Int {
+		return n
+	}
+}
